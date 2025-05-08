6f8f5695308939abf4ca5cff60cb85f0d6be37d3
snackcookie <fghkk12d@gmail.com>
2025-05-08 10:15:24 +0900

    test

--- src/main/java/com/nhnacademy/front/IndexController.java
+++ src/main/java/com/nhnacademy/front/IndexController.java
@@ -1,4 +1,12 @@
 package com.nhnacademy.front;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+    @GetMapping("/")
+    public String homePage() {
+        return "index"; // templates/home.html 을 찾아 렌더링
+    }
 }
