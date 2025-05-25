f228cc87e07830f77bbab89edfd0ea1cec5c88fb
junopo <junopo@naver.com>
2025-05-25 16:31:00 +0900

    feat: 도서 등록 페이지

--- src/main/java/com/nhnacademy/front/book/controller/BookController.java
+++ src/main/java/com/nhnacademy/front/book/controller/BookController.java
@@ -14,4 +14,19 @@ public class BookController {
     public String getBookDetail() {
         return "book/book-detail";
     }
+
+    @GetMapping("/book-register")
+    public String getBookRegister() {
+        return "book/book-register";
+    }
+
+    @GetMapping("/test-register")
+    public String getTestRegister() {
+        return "book/test-register";
+    }
+
+    @GetMapping("/test-author")
+    public String getTestAuthor() {
+        return "book/test-author";
+    }
 }
