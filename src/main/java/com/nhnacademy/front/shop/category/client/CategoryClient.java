871fff54093484334d4ff77b23328544e738d73d
junopo <junopo@naver.com>
2025-05-30 14:25:31 +0900

    feat: 카테고리 추가 / 계층 구조

--- src/main/java/com/nhnacademy/front/shop/category/client/CategoryClient.java
+++ src/main/java/com/nhnacademy/front/shop/category/client/CategoryClient.java
@@ -2,10 +2,13 @@ package com.nhnacademy.front.shop.category.client;
 
 import com.nhnacademy.front.common.dto.CommonResponse;
 import com.nhnacademy.front.common.dto.PageResponse;
+import com.nhnacademy.front.shop.category.client.dto.CategoryCreateRequest;
 import com.nhnacademy.front.shop.category.client.dto.CategoryResponse;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "categoryClient", url = "${feign.url.shop}")
 public interface CategoryClient {
@@ -15,4 +18,7 @@ public interface CategoryClient {
 
     @GetMapping("/categories")
     CommonResponse<PageResponse<CategoryResponse>> getCategories();
+
+    @PostMapping("/categories")
+    CommonResponse<CategoryResponse> createCategory(@ModelAttribute CategoryCreateRequest request);
 }
