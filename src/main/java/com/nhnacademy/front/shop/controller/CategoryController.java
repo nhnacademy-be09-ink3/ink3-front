85972f58f915583f3f5e6bb40d25f47d6c81f6ed
junopo <junopo@naver.com>
2025-05-30 14:25:31 +0900

    feat: 카테고리 추가 / 계층 구조

--- src/main/java/com/nhnacademy/front/shop/controller/CategoryController.java
+++ src/main/java/com/nhnacademy/front/shop/controller/CategoryController.java
@@ -3,12 +3,15 @@ package com.nhnacademy.front.shop.controller;
 import com.nhnacademy.front.common.dto.CommonResponse;
 import com.nhnacademy.front.common.dto.PageResponse;
 import com.nhnacademy.front.shop.category.client.CategoryClient;
+import com.nhnacademy.front.shop.category.client.dto.CategoryCreateRequest;
 import com.nhnacademy.front.shop.category.client.dto.CategoryResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -25,5 +28,11 @@ public class CategoryController {
         model.addAttribute("categories", categories.data());
 
         return "admin/categories";
+    }
+
+    @PostMapping("/admin/categories")
+    public String createCategory(@ModelAttribute CategoryCreateRequest request) {
+        categoryClient.createCategory(request);
+        return "redirect:/admin/categories";
     }
 }
