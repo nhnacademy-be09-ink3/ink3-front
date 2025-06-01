ffc9f4d83216a48ad88897f0382acbebdfbc9fdf
Jihyun3478 <leejihyun3478@gmail.com>
2025-06-01 16:37:40 +0900

    hotfix: Multipart 관련 오류 수정

--- src/main/java/com/nhnacademy/front/shop/controller/ReviewController.java
+++ src/main/java/com/nhnacademy/front/shop/controller/ReviewController.java
@@ -2,16 +2,16 @@ package com.nhnacademy.front.shop.controller;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.nhnacademy.front.shop.review.client.ReviewClient;
-import com.nhnacademy.front.shop.review.dto.ReviewFormUpdateRequest;
+import com.nhnacademy.front.shop.review.dto.ReviewUpdateRequest;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,15 +29,17 @@ public class ReviewController {
     //     return "redirect:/books/" + bookId;
     // }
 
-    @PostMapping("/{bookId}/reviews/{reviewId}")
+    @PostMapping(value = "/{bookId}/reviews/{reviewId}/update", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String updateReview(@PathVariable Long bookId,
         @PathVariable Long reviewId,
-        @ModelAttribute ReviewFormUpdateRequest form, List<MultipartFile> images) {
-        reviewClient.updateReview(reviewId, form.toRequest(), images);
+        @RequestPart("review") ReviewUpdateRequest request,
+        @RequestPart(value = "images", required = false) List<MultipartFile> images) {
+
+        reviewClient.updateReview(reviewId, request, images);
         return "redirect:/books/" + bookId;
     }
 
-    @GetMapping("/{book-id}/reviews/{review-id}")
+    @PostMapping("/{book-id}/reviews/{review-id}/delete")
     public String deleteReview(@PathVariable(name = "book-id") Long bookId,
         @PathVariable(name = "review-id") Long reviewId) {
         reviewClient.deleteReview(reviewId);
--- src/main/java/com/nhnacademy/front/shop/review/dto/ReviewUpdateRequest.java
+++ src/main/java/com/nhnacademy/front/shop/review/dto/ReviewUpdateRequest.java
@@ -1,8 +1,21 @@
 package com.nhnacademy.front.shop.review.dto;
 
-public record ReviewUpdateRequest(
-    String title,
-    String content,
-    int rating
-) {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviewUpdateRequest {
+    private String title;
+    private String content;
+    private int rating;
+
+    public ReviewUpdateRequest() {
+    }
+
+    public ReviewUpdateRequest(String title, String content, int rating) {
+        this.title = title;
+        this.content = content;
+        this.rating = rating;
+    }
 }
