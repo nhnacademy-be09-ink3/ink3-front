4a31509977acd5c7de4672997431a41f000dca59
Jihyun3478 <leejihyun3478@gmail.com>
2025-06-02 02:16:32 +0900

    feat: 메인페이지 및 베스트셀러, 신간, 추천 도서 목록 페이지 구현

--- src/main/java/com/nhnacademy/front/shop/book/client/BookClient.java
+++ src/main/java/com/nhnacademy/front/shop/book/client/BookClient.java
@@ -3,15 +3,18 @@ package com.nhnacademy.front.shop.book.client;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nhnacademy.front.common.dto.CommonResponse;
 import com.nhnacademy.front.common.dto.PageResponse;
-import com.nhnacademy.front.shop.book.client.dto.BookCreateRequest;
-import com.nhnacademy.front.shop.book.client.dto.BookResponse;
+import com.nhnacademy.front.shop.book.dto.BookCreateRequest;
+import com.nhnacademy.front.shop.book.dto.BookResponse;
+import com.nhnacademy.front.shop.book.dto.MainBookResponse;
 
 @FeignClient(name = "bookClient", url = "${feign.url.shop}")
 public interface BookClient {
@@ -21,6 +24,24 @@ public interface BookClient {
 
     @GetMapping("/books")
     CommonResponse<PageResponse<BookResponse>> getBooks();
+
+    @GetMapping("/books/bestseller")
+    CommonResponse<PageResponse<MainBookResponse>> getTop5BestsellerBooks();
+
+    @GetMapping("/books/bestseller-all")
+    CommonResponse<PageResponse<MainBookResponse>> getAllBestsellerBooks(@RequestParam int page, @RequestParam int size);
+
+    @GetMapping("/books/new")
+    CommonResponse<PageResponse<MainBookResponse>> getTop5NewBooks();
+
+    @GetMapping("/books/new-all")
+    CommonResponse<PageResponse<MainBookResponse>> getAllNewBooks(@RequestParam int page, @RequestParam int size);
+
+    @GetMapping("/books/recommend")
+    CommonResponse<PageResponse<MainBookResponse>> getTop5RecommendedBooks();
+
+    @GetMapping("/books/recommend-all")
+    CommonResponse<PageResponse<MainBookResponse>> getAllRecommendedBooks(@RequestParam int page, @RequestParam int size);
 
     @GetMapping("/books/{bookId}")
     CommonResponse<BookResponse> getBookDetail(@PathVariable Long bookId);
