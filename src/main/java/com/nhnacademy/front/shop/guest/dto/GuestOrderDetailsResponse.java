33ae4ce25ffc8941d375690874c1c6418f06ab63
권용민 <kwonym0914@gmail.com>
2025-06-04 21:57:47 +0900

    feat: 비회원 조회 및 주문/결제 로직 완료

---
+++ src/main/java/com/nhnacademy/front/shop/guest/dto/GuestOrderDetailsResponse.java
@@ -0,0 +1,30 @@
+package com.nhnacademy.front.shop.guest.dto;
+
+import com.nhnacademy.front.shop.order.dto.OrderStatus;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class GuestOrderDetailsResponse {
+    private Long orderId;
+    private Long orderUUId;
+    private OrderStatus status;
+    private LocalDateTime orderedAt;
+    private String ordererName;
+    private String ordererPhone;
+    private LocalDate preferredDeliveryDate;
+    private LocalDateTime deliveredAt;
+    private String recipientName;
+    private String recipientPhone;
+    private Integer postalCode;
+    private String defaultAddress;
+    private String extraAddress;
+    private Integer shippingFee;
+    private String shippingCode;
+}
