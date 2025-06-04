9fe54d287b69016fe7c4b81ae400609fde7d7f1f
dusk1006 <inyupj@gmail.com>
2025-06-04 13:00:16 +0900

    중간저장
    # Conflicts:
    #	src/main/resources/static/css/output.css

--- src/test/java/com/nhnacademy/front/FrontApplicationTests.java
+++ src/test/java/com/nhnacademy/front/FrontApplicationTests.java
@@ -2,8 +2,10 @@ package com.nhnacademy.front;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class FrontApplicationTests {
 
 	@Test
