14e9cc21a0c96f6946508e9ad151a647a3ea9509
권용민 <kwonym0914@gmail.com>
2025-06-08 13:55:09 +0900

    hotfix: 0원 결제 시 오류 해결

--- src/main/java/com/nhnacademy/front/shop/payment/client/PaymentClient.java
+++ src/main/java/com/nhnacademy/front/shop/payment/client/PaymentClient.java
@@ -2,6 +2,7 @@ package com.nhnacademy.front.shop.payment.client;
 import com.nhnacademy.front.common.dto.CommonResponse;
 import com.nhnacademy.front.shop.payment.dto.PaymentConfirmRequest;
 import com.nhnacademy.front.shop.payment.dto.PaymentResponse;
+import com.nhnacademy.front.shop.payment.dto.ZeroPaymentRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "shop-service", url = "${feign.url.shop}")
 public interface PaymentClient {
+    // 결제 생성 (0원 결제 시)
+    @PostMapping("/payments")
+    CommonResponse<PaymentResponse> createZeroPayment(@RequestBody ZeroPaymentRequest request);
     // payment 결제 승인 요청
     @PostMapping("/payments/confirm")
     CommonResponse<PaymentResponse> confirmPayment(@RequestBody PaymentConfirmRequest request);
