b210981fb4439e9e46fa75ebe83aef61992d0d5c
junopo <junopo@naver.com>
2025-06-09 11:30:02 +0900

    hotfix: 도서 등록, 수정 시 작가, 출판사, 태그의 모든 정보를 가져오지 못하는 문제 일시적으로 해결

---
+++ src/main/java/com/nhnacademy/front/shop/book/client/AladinClient.java
@@ -0,0 +1,4 @@
+package com.nhnacademy.front.shop.book.client;
+
+public interface AladinClient {
+}
--- src/main/java/com/nhnacademy/front/shop/book/client/BookClient.java
+++ src/main/java/com/nhnacademy/front/shop/book/client/BookClient.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.front.shop.book.client;
 
 import com.nhnacademy.front.shop.book.dto.BookResponse;
-import com.nhnacademy.front.shop.book.dto.BookUpdateRequest;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nhnacademy.front.common.dto.CommonResponse;
