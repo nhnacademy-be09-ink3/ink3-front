a929f0a8dd5fa8542bcfbc78004fe1abf813c399
junopo <junopo@naver.com>
2025-06-09 12:56:31 +0900

    feat: 외부 api 사용 구현 중

--- src/main/java/com/nhnacademy/front/shop/book/client/AladinClient.java
+++ src/main/java/com/nhnacademy/front/shop/book/client/AladinClient.java
@@ -1,4 +1,23 @@
 package com.nhnacademy.front.shop.book.client;
 
+import com.nhnacademy.front.common.dto.CommonResponse;
+import com.nhnacademy.front.common.dto.PageResponse;
+import com.nhnacademy.front.shop.book.dto.AladinBookResponse;
+import com.nhnacademy.front.shop.book.dto.BookRegisterRequest;
+import com.nhnacademy.front.shop.book.dto.BookResponse;
+import jakarta.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "aladinClient", url = "${feign.url.shop}")
 public interface AladinClient {
+
+    @GetMapping("/aladin")
+    CommonResponse<PageResponse<AladinBookResponse>> getBooksByKeyword(@RequestParam String keyword, @RequestParam int page, @RequestParam int size);
+
+    @PostMapping(value = "/aladin/register-book")
+    CommonResponse<BookResponse> registerBookByAladin(@RequestBody @Valid BookRegisterRequest request);
 }
--- src/main/java/com/nhnacademy/front/shop/controller/AladinController.java
+++ src/main/java/com/nhnacademy/front/shop/controller/AladinController.java
@@ -1,4 +1,43 @@
 package com.nhnacademy.front.shop.controller;
 
+import com.nhnacademy.front.common.dto.CommonResponse;
+import com.nhnacademy.front.common.dto.PageResponse;
+import com.nhnacademy.front.shop.author.client.dto.AuthorResponse;
+import com.nhnacademy.front.shop.book.client.AladinClient;
+import com.nhnacademy.front.shop.book.dto.AladinBookResponse;
+import com.nhnacademy.front.util.PageUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequiredArgsConstructor
+@Controller
 public class AladinController {
+
+    private final AladinClient aladinClient;
+
+    @GetMapping("/admin/aladin-search")
+    public String getAladinBookRegister() {
+        return "admin/book/aladin-search";
+    }
+
+
+    @GetMapping("/admin/aladin")
+    public String getBooksByAladin(@RequestParam String keyword,
+                                   @RequestParam(required = false, defaultValue = "0") int page,
+                                   Model model) {
+        CommonResponse<PageResponse<AladinBookResponse>> response = aladinClient.getBooksByKeyword(keyword, page, 10);
+        PageResponse<AladinBookResponse> aladinBooks = response.data();
+        PageUtil.PageInfo pageInfo = PageUtil.calculatePageRange(
+                aladinBooks.page(), aladinBooks.totalPages(), 5
+        );
+
+        model.addAttribute("currentPage", "aladinBooks");
+        model.addAttribute("aladinBooks", aladinBooks);
+        model.addAttribute("pageInfo", pageInfo);
+
+        return "admin/book/aladin-register";
+    }
 }
