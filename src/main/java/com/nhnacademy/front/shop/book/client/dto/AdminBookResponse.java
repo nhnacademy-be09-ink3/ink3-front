933240c4b30d5e7af3f3f2ad945e546acf9d719f
KastanEr <skuller320@naver.com>
2025-06-13 17:54:16 +0900

    refactor: 카테고리 리팩토링

---
+++ src/main/java/com/nhnacademy/front/shop/book/client/dto/AdminBookResponse.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.front.shop.book.client.dto;
+
+import com.nhnacademy.front.shop.book.dto.BookStatus;
+import java.time.LocalDate;
+
+public record AdminBookResponse(
+        Long id,
+        String isbn,
+        String title,
+        String publisher,
+        LocalDate publishedAt,
+        int originalPrice,
+        int salePrice,
+        int discountRate,
+        int quantity,
+        boolean isPackable,
+        double averageRating,
+        String thumbnailUrl,
+        BookStatus status
+) {
+}
