aedce49f9f3c03699405bc5caca51cf12f9fb374
KastanEr <skuller320@naver.com>
2025-06-16 13:16:02 +0900

    feat: elastic search 및 헤더 카테고리 구현

--- src/main/java/com/nhnacademy/front/shop/search/client/BookSearchClient.java
+++ src/main/java/com/nhnacademy/front/shop/search/client/BookSearchClient.java
@@ -1,4 +1,27 @@
 package com.nhnacademy.front.shop.search.client;
 
+import com.nhnacademy.front.common.dto.CommonResponse;
+import com.nhnacademy.front.common.dto.PageResponse;
+import com.nhnacademy.front.shop.book.dto.BookPreviewResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "bookSearchClient", url = "${feign.url.shop}/search/books")
 public interface BookSearchClient {
+    @GetMapping("/by-keyword")
+    CommonResponse<PageResponse<BookPreviewResponse>> searchBooksByKeyword(
+            @RequestParam String keyword,
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size,
+            @RequestParam(required = false, defaultValue = "POPULARITY") String sort
+    );
+
+    @GetMapping("/by-category")
+    CommonResponse<PageResponse<BookPreviewResponse>> searchBooksByCategory(
+            @RequestParam String category,
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size,
+            @RequestParam(required = false, defaultValue = "POPULARITY") String sort
+    );
 }
--- src/main/java/com/nhnacademy/front/shop/search/client/BookSortOption.java
+++ src/main/java/com/nhnacademy/front/shop/search/client/BookSortOption.java
@@ -1,20 +1,17 @@
-package shop.ink3.api.elastic.model;
+package com.nhnacademy.front.shop.search.client;
 
-import co.elastic.clients.elasticsearch._types.SortOrder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum BookSortOption {
-    POPULARITY("인기도", "popularityScore", SortOrder.Desc),
-    NEWEST("신상품", "publishedAt", SortOrder.Desc),
-    LOWEST_PRICE("최저가", "price", SortOrder.Asc),
-    HIGHEST_PRICE("최고가", "price", SortOrder.Desc),
-    RATING("평점순", "rating", SortOrder.Desc),
-    REVIEW("리뷰순", "reviewCount", SortOrder.Desc);
+    POPULARITY("인기도"),
+    NEWEST("신상품"),
+    LOWEST_PRICE("최저가"),
+    HIGHEST_PRICE("최고가"),
+    RATING("평점순"),
+    REVIEW("리뷰순");
 
-    private final String description;
-    private final String sortField;
-    private final SortOrder sortOrder;
+    private final String label;
 }
